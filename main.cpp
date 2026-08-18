e83694701181143935b25e4e13ea3ba132b1d4a0
Austin Song <astt4506@gmail.com>
2026-08-18 02:33:35 -0500

    Add code from test.cpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -68,6 +68,74 @@ void clear_terminal()
     }
 #endif
 
+enum Key {
+    Not_Recognized,
+    Enter,
+    Up_Arrow,
+    Down_Arrow,
+    Left_Arrow,
+    Right_Arrow,
+};
+
+#ifdef _WIN32
+    #include <conio.h>
+
+    Key get_special_key() {
+        int ch = _getch();
+
+        if (ch == '\r') {
+            return Key::Enter;
+        }
+
+        if (ch == 0 || ch == 224) {
+            switch (_getch()) {
+                case 72: return Key::Up_Arrow;
+                case 80: return Key::Down_Arrow;
+                case 75: return Key::Left_Arrow;
+                case 77: return Key::Right_Arrow;
+            }
+        }
+
+        return Key::Not_Recognized;
+    }
+#else
+    #include <termios.h>
+    #include <unistd.h>
+    #include <cstdio>
+
+    Key get_special_key() {
+        termios oldt, newt;
+
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+        int ch = getchar();
+
+        Key result = Key::Not_Recognized;
+
+        if (ch == '\n' || ch == '\r') {
+            result = Key::Enter;
+        }
+        else if (ch == 27) { // ESC sequence
+            if (getchar() == '[') {
+                switch (getchar()) {
+                    case 'A': result = Key::Up_Arrow;    break;
+                    case 'B': result = Key::Down_Arrow;  break;
+                    case 'C': result = Key::Right_Arrow; break;
+                    case 'D': result = Key::Left_Arrow;  break;
+                }
+            }
+        }
+
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+        return result;
+    }
+#endif
+
 int get_random_number_inclusive(const int& bound_1, const int& bound_2) {
     std::random_device rd;
     const int lower_bound = std::min(bound_1, bound_2);
